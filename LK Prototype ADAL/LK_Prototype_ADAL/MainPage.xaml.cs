8fa1389311eb45a01c4f3bf4ffdbd212f30354d6
jkhelland <johnkennethhelland@gmail.com>
2017-01-12 15:14:22 +0100

    Add project files.

---
+++ LK Prototype ADAL/LK_Prototype_ADAL/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LK_Prototype_ADAL
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
